35e1b534e1c7fc4c3f336bcf56305a4d7a483599
Mauricio Rojas <mrojasb2000@gmail.com>
2019-05-03 12:20:00 -0400

    Adds exercises with Rust.

---
+++ chapter2/change_constant.rs
@@ -0,0 +1,8 @@
+// see chapter2/change_constant.rs
+
+static MAX_HEALTH: i32 = 100;
+
+fn main(){
+    //MAX_HEALTH = 200;
+    //error: aborting due to previous error
+}
---
+++ chapter2/formatting.rs
@@ -0,0 +1,7 @@
+// see chapter2/formatting.rs
+
+fn main(){
+    let dec = 3.2f32;
+    println!("{}", dec);
+    println!("{:+007.2}", dec );
+}
