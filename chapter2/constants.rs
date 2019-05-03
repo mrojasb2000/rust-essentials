53457e2ccd831e17c23dd2ea42f57c31e07af923
Mauricio Rojas <mrojasb2000@gmail.com>
2019-05-03 10:22:23 -0400

    Adds more constants definitions.

--- chapter2/constants.rs
+++ chapter2/constants.rs
@@ -8,4 +8,8 @@ fn main(){
     println!("The Game you are playing is called {}.", GAME_NAME);
     println!("You start with {} health points.", MAX_HEALTH);
     println!("In the Game {0} you start with {1} % health, yes you rea", MAX_HEALTH, GAME_NAME);
+    println!("You have {points} % health", points = 70);
+    println!("MAX_HEALTH is {:x} in hexadecimal", MAX_HEALTH);
+    println!("MAX_HEALTH is {:b} in binary", MAX_HEALTH);
+    println!("Two written in binary {0:b}", 2);
 }
