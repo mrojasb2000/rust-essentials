c1f492aca874f7532c2e4392f4feece3bd199db4
Mauricio Rojas <mrojasb2000@gmail.com>
2019-05-03 10:14:45 -0400

    Adds more constants definitions.

--- chapter2/constants.rs
+++ chapter2/constants.rs
@@ -7,4 +7,5 @@ const MYPI: f32 = 3.14;
 fn main(){
     println!("The Game you are playing is called {}.", GAME_NAME);
     println!("You start with {} health points.", MAX_HEALTH);
+    println!("In the Game {0} you start with {1} % health, yes you rea", MAX_HEALTH, GAME_NAME);
 }
