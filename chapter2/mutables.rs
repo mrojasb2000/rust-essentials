4399c7fcce65168148cb49cec6bc0dfa5659cdd6
Mauricio Rojas <mrojasb2000@gmail.com>
2019-05-04 21:21:17 -0400

    Adds variables mutables.

--- chapter2/mutables.rs
+++ chapter2/mutables.rs
@@ -5,4 +5,9 @@ fn main(){
     println!("Fuel: {}", fuel);
     fuel = 60;
     println!("Fuel: {}", fuel);
+
+    let x = 42u8;
+    println!("x: {}", x);
+    let magic_number = 3.14f64;
+    println!("Mgic Number: {}", magic_number);
 }
